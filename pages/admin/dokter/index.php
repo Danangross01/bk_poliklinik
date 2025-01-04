adb20600a423fc4090c19b298c13b67080d7b419
Danang Prasetya <114502449+Danangross01@users.noreply.github.com>
2025-01-04 11:50:50 +0700

    Update index.php

--- pages/admin/dokter/index.php
+++ pages/admin/dokter/index.php
@@ -1,217 +1,201 @@
 <?php
+ob_start(); // Mengaktifkan buffer output
 include_once("../../../config/conn.php");
 session_start();
 
-//Mengecek apakah sesi login telah diatur
+// Mengecek apakah sesi login telah diatur
 if (isset($_SESSION['login'])) {
-  $_SESSION['login'] = true;
+    $_SESSION['login'] = true;
 } else {
-  echo "<meta http-equiv='refresh' content='0; url=../auth/login.php'>";
-  die();
+    echo "<meta http-equiv='refresh' content='0; url=../auth/login.php'>";
+    exit;
 }
 
 $nama = $_SESSION['username'];
 $akses = $_SESSION['akses'];
 
-//Hanya pengguna dengan akses admin 
+// Hanya pengguna dengan akses admin
 if ($akses != 'admin') {
-  echo "<meta http-equiv='refresh' content='0; url=../..'>";
-  die();
+    echo "<meta http-equiv='refresh' content='0; url=../..'>";
+    exit;
 }
-?>
-<?php
-$title = 'Poliklinik | Obat';
-// Breadcrumb section / Pembuatan Breadcrumb dan Judul
-ob_start();?>
+
+$title = 'Poliklinik | Dokter';
+
+// Breadcrumb section
+ob_start(); ?>
 <ol class="breadcrumb float-sm-right">
-  <li class="breadcrumb-item"><a href="<?= $base_admin; ?>">Home</a></li>
-  <li class="breadcrumb-item active">Obat</li>
+    <li class="breadcrumb-item"><a href="<?= $base_admin; ?>">Home</a></li>
+    <li class="breadcrumb-item active">Dokter</li>
 </ol>
 <?php
 $breadcrumb = ob_get_clean();
-ob_flush();
 
 // Title Section
-ob_start();?>
+ob_start(); ?>
 Tambah / Edit Dokter
 <?php
 $main_title = ob_get_clean();
-ob_flush();
 
 // Content section
 ob_start();
 ?>
 <form class="form col" method="POST" action="" name="myForm" onsubmit="return(validate());">
-        <?php
-        $nama = '';
-        $alamat = '';
-        $no_hp = '';
-        $id_poli = 0;
-        //Pre-populasi Form: Jika ada parameter id, data dokter akan diambil dari database
-        if (isset($_GET['id'])) {
-            try {
-                $stmt = $pdo->prepare("SELECT * FROM dokter WHERE id = :id");
-                $stmt->bindParam(':id', $_GET['id']);
-                $stmt->execute();
+    <?php
+    $nama = '';
+    $alamat = '';
+    $no_hp = '';
+    $id_poli = 0;
 
-                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    $nama = $row['nama'];
-                    $alamat = $row['alamat'];
-                    $no_hp = $row['no_hp'];
-                    $id_poli = $row['id_poli'];
-                }
-            } catch (PDOException $e) {
-                echo "Error: " . $e->getMessage();
+    // Pre-populasi Form
+    if (isset($_GET['id'])) {
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM dokter WHERE id = :id");
+            $stmt->bindParam(':id', $_GET['id']);
+            $stmt->execute();
+
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $nama = $row['nama'];
+                $alamat = $row['alamat'];
+                $no_hp = $row['no_hp'];
+                $id_poli = $row['id_poli'];
             }
-        ?>  
-        <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
-        <?php
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
         }
-        ?>
-        
-        <div class="row mt-3">
-            <label for="nama" class="form-label fw-bold">
-                Nama Dokter
-            </label>
-            <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama Dokter" value="<?php echo $nama ?>">
-        </div>
-        <div class="row mt-3">
-            <label for="alamat" class="form-label fw-bold">
-                alamat
-            </label>
-            <input type="text" class="form-control" name="alamat" id="alamat" placeholder="alamat" value="<?php echo $alamat ?>">
-        </div>
-        <div class="row mt-3">
-            <label for="no_hp" class="form-label fw-bold">
-                no_hp
-            </label>
-            <input type="text" class="form-control" name="no_hp" id="no_hp" placeholder="no_hp" value="<?php echo $no_hp ?>">
-        </div>
-
-        <div class="row mt-3">
-            <label for="id_poli" class="form-label fw-bold">
-                Poli
-            </label>  
-            <select class="form-control" name="id_poli" id="id_poli">
-                <?php
-                $stmt = $pdo->query("SELECT * FROM poli");
-                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    $selected = ($id_poli == $row['id']) ? 'selected' : '';
-                    echo "<option value='" . $row['id'] . "' $selected>" . $row['nama_poli'] . "</option>";
-                }
-                ?>
-            </select>
-        </div>
-
-        <div class="row d-flex mt-3 mb-3">
-          <button type="submit" class="btn btn-primary" style="width: 3cm;" name="simpan">Simpan</button>
-        </div>
+    ?>
+    <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
+    <?php
+    }
+    ?>
+    <div class="row mt-3">
+        <label for="nama" class="form-label fw-bold">Nama Dokter</label>
+        <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama Dokter" value="<?php echo $nama ?>">
+    </div>
+    <div class="row mt-3">
+        <label for="alamat" class="form-label fw-bold">Alamat</label>
+        <input type="text" class="form-control" name="alamat" id="alamat" placeholder="Alamat" value="<?php echo $alamat ?>">
+    </div>
+    <div class="row mt-3">
+        <label for="no_hp" class="form-label fw-bold">No HP</label>
+        <input type="text" class="form-control" name="no_hp" id="no_hp" placeholder="No HP" value="<?php echo $no_hp ?>">
+    </div>
+    <div class="row mt-3">
+        <label for="id_poli" class="form-label fw-bold">Poli</label>
+        <select class="form-control" name="id_poli" id="id_poli">
+            <?php
+            $stmt = $pdo->query("SELECT * FROM poli");
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $selected = ($id_poli == $row['id']) ? 'selected' : '';
+                echo "<option value='" . $row['id'] . "' $selected>" . $row['nama_poli'] . "</option>";
+            }
+            ?>
+        </select>
+    </div>
+    <div class="row d-flex mt-3 mb-3">
+        <button type="submit" class="btn btn-primary" style="width: 3cm;" name="simpan">Simpan</button>
+    </div>
 </form>
-
 <div class="row d-flex mt-3 mb-3">
-  <a href="<?= $base_admin.'/dokter' ?>">
-    <button class="btn btn-secondary ml-2" style="width: 3cm;">Reset</button>
-  </a>
+    <a href="<?= $base_admin . '/dokter' ?>">
+        <button class="btn btn-secondary ml-2" style="width: 3cm;">Reset</button>
+    </a>
 </div>
 
 <div class="card">
-  <div class="card-header">
-    <h3 class="card-title">Dokter</h3>
-  </div>
-  <div class="card-body">
-    <table id="example1" class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">No</th>
-          <th scope="col">Nama</th>
-          <th scope="col">Alamat</th>
-          <th scope="col">No. Hp</th>
-          <th scope="col">Poli</th>
-          <th scope="col">Aksi</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        //Menampilkan Data Dokter 
-        $result = $pdo->query("SELECT * FROM dokter");
-        $no = 1;
-        while ($data = $result->fetch(PDO::FETCH_ASSOC)) {
-        ?>
-          <tr>
-              <td><?php echo $no++ ?></td>
-              <td><?php echo $data['nama'] ?></td>
-              <td><?php echo $data['alamat'] ?></td>
-              <td><?php echo $data['no_hp'] ?></td>
-              <td>
+    <div class="card-header">
+        <h3 class="card-title">Dokter</h3>
+    </div>
+    <div class="card-body">
+        <table id="example1" class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Alamat</th>
+                    <th scope="col">No. HP</th>
+                    <th scope="col">Poli</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
                 <?php
-                //Relasi Poli: Nama poli diambil dari tabel poli menggunakan ID poli yang ada di tabel dokter
-                $id_poli = $data['id_poli'];
-                $poli = $pdo->query("SELECT * FROM poli WHERE id = $id_poli");
+                $result = $pdo->query("SELECT * FROM dokter");
                 $no = 1;
-                while ($data_poli = $poli->fetch(PDO::FETCH_ASSOC)) {
-                  echo $data_poli['nama_poli'];
+                while ($data = $result->fetch(PDO::FETCH_ASSOC)) {
+                ?>
+                    <tr>
+                        <td><?php echo $no++; ?></td>
+                        <td><?php echo $data['nama']; ?></td>
+                        <td><?php echo $data['alamat']; ?></td>
+                        <td><?php echo $data['no_hp']; ?></td>
+                        <td>
+                            <?php
+                            $id_poli = $data['id_poli'];
+                            $poli = $pdo->query("SELECT * FROM poli WHERE id = $id_poli");
+                            while ($data_poli = $poli->fetch(PDO::FETCH_ASSOC)) {
+                                echo $data_poli['nama_poli'];
+                            }
+                            ?>
+                        </td>
+                        <td>
+                            <a class="btn btn-success" href="index.php?page=dokter&id=<?php echo $data['id']; ?>">Ubah</a>
+                            <a class="btn btn-danger" href="index.php?page=dokter&id=<?php echo $data['id']; ?>&aksi=hapus">Hapus</a>
+                        </td>
+                    </tr>
+                <?php
                 }
                 ?>
-                
-              </td>
-              <td>
-                  <a class="btn btn-success" href="index.php?page=obat&id=<?php echo $data['id'] ?>">Ubah</a>
-                  <a class="btn btn-danger" href="index.php?page=obat&id=<?php echo $data['id'] ?>&aksi=hapus">Hapus</a>
-              </td>
-          </tr>
-      <?php
-      }
-      ?>
-      </tbody>
-    </table>
-    <?php
-    //Simpan Data: Jika form disubmit
+            </tbody>
+        </table>
+
+        <?php
+        // Simpan Data
         if (isset($_POST['simpan'])) {
-        if (isset($_POST['id'])) {
-            $stmt = $pdo->prepare("UPDATE dokter SET 
-                                    nama = :nama,
-                                    alamat = :alamat,
-                                    no_hp = :no_hp,
-                                    id_poli = :id_poli
-                                    WHERE
-                                    id = :id");
+            if (isset($_POST['id'])) {
+                $stmt = $pdo->prepare("UPDATE dokter SET 
+                                        nama = :nama,
+                                        alamat = :alamat,
+                                        no_hp = :no_hp,
+                                        id_poli = :id_poli
+                                        WHERE id = :id");
 
-            $stmt->bindParam(':nama', $_POST['nama'], PDO::PARAM_STR);
-            $stmt->bindParam(':alamat', $_POST['alamat'], PDO::PARAM_STR);
-            $stmt->bindParam(':no_hp', $_POST['no_hp'], PDO::PARAM_INT);
-            $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_INT);
-            $stmt->bindParam(':id_poli', $_POST['id_poli'], PDO::PARAM_INT);
-            $stmt->execute();
+                $stmt->bindParam(':nama', $_POST['nama'], PDO::PARAM_STR);
+                $stmt->bindParam(':alamat', $_POST['alamat'], PDO::PARAM_STR);
+                $stmt->bindParam(':no_hp', $_POST['no_hp'], PDO::PARAM_STR);
+                $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_INT);
+                $stmt->bindParam(':id_poli', $_POST['id_poli'], PDO::PARAM_INT);
+                $stmt->execute();
 
-            header('Location:index.php');
+                header('Location: index.php');
+                exit;
+            } else {
+                $stmt = $pdo->prepare("INSERT INTO dokter(nama, alamat, no_hp, id_poli) 
+                                        VALUES (:nama, :alamat, :no_hp, :id_poli)");
 
-        } else {
-            $stmt = $pdo->prepare("INSERT INTO dokter(nama, alamat, no_hp, id_poli) 
-                                    VALUES (:nama, :alamat, :no_hp, :id_poli)");
+                $stmt->bindParam(':nama', $_POST['nama'], PDO::PARAM_STR);
+                $stmt->bindParam(':alamat', $_POST['alamat'], PDO::PARAM_STR);
+                $stmt->bindParam(':no_hp', $_POST['no_hp'], PDO::PARAM_STR);
+                $stmt->bindParam(':id_poli', $_POST['id_poli'], PDO::PARAM_INT);
+                $stmt->execute();
 
-            $stmt->bindParam(':nama', $_POST['nama'], PDO::PARAM_STR);
-            $stmt->bindParam(':alamat', $_POST['alamat'], PDO::PARAM_STR);
-            $stmt->bindParam(':no_hp', $_POST['no_hp'], PDO::PARAM_INT);
-            $stmt->bindParam(':id_poli', $_POST['id_poli'], PDO::PARAM_INT);
-            $stmt->execute();
-
-            header('Location:index.php');
+                header('Location: index.php');
+                exit;
+            }
         }
-    }//Hapus Data: Jika parameter aksi=hapus ada
-    if (isset($_GET['aksi']) && $_GET['aksi'] == 'hapus') {
-    $stmt = $pdo->prepare("DELETE FROM dokter WHERE id = :id");
-    $stmt->bindParam(':id', $_GET['id']);
-    $stmt->execute();
 
-    header('Location:index.php');
-    
-    }
-    ?>
-  </div>
+        // Hapus Data
+        if (isset($_GET['aksi']) && $_GET['aksi'] == 'hapus') {
+            $stmt = $pdo->prepare("DELETE FROM dokter WHERE id = :id");
+            $stmt->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+            $stmt->execute();
+
+            header('Location: index.php');
+            exit;
+        }
+        ?>
+    </div>
 </div>
 <?php
 $content = ob_get_clean();
-ob_flush();
-?>
-
-<?php include '../../../layouts/index.php'; ?>
+include '../../../layouts/index.php';
